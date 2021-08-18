6a7abbf237036c57b570f54ec29c8ba6610c1670
Tomas Carignano <tomascarignano98@gmail.com>
2021-08-18 11:52:29 -0300

    Made sure to have a default bg image in case the Promise is rejected.

--- main.js
+++ main.js
@@ -6,4 +6,11 @@ fetch(
     console.log(data);
     document.body.style.backgroundImage = `url(${data.urls.full})`;
     document.getElementById("img-author").textContent = `By: ${data.user.name}`;
+  })
+  .catch((error) => {
+    console.log("Using default bg image due to an error.");
+    console.log(error.stack);
+
+    document.body.style.backgroundImage = `url("https://images.unsplash.com/photo-1504198266287-1659872e6590?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjkyOTgwMjE&ixlib=rb-1.2.1&q=85")`;
+    document.getElementById("img-author").textContent = `By: Sarah Dorweiler`;
   });
