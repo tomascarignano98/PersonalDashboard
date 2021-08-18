f83f7d65a585c38646cf2a96991578e87413105c
Tomas Carignano <tomascarignano98@gmail.com>
2021-08-18 15:36:53 -0300

    Get Crypcurrency data.

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
+// Get background image
 fetch(
   "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
 )
-  .then((response) => response.json())
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`${response.status}`);
+    }
+
+    return response.json();
+  })
   .then((data) => {
     console.log(data);
     document.body.style.backgroundImage = `url(${data.urls.full})`;
@@ -14,3 +21,24 @@ fetch(
     document.body.style.backgroundImage = `url("https://images.unsplash.com/photo-1504198266287-1659872e6590?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjkyOTgwMjE&ixlib=rb-1.2.1&q=85")`;
     document.getElementById("img-author").textContent = `By: Sarah Dorweiler`;
   });
+
+// Get Crypto data
+fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`${response.status}`);
+    }
+
+    return response.json();
+  })
+  .then((data) => {
+    const price = data.market_data.current_price.usd;
+    const logo = data.image.thumb;
+    console.log(data);
+
+    document.querySelector(".crypto-data").innerHTML = `
+    <img src="${logo}" alt="" />
+    <span>$${price}</span>
+    `;
+  })
+  .catch((error) => console.log(error.stack));
