765a0dffec7115869d2727f7cfe7490f559ddd7e
Tomas Carignano <tomascarignano98@gmail.com>
2021-08-19 12:32:53 -0300

    Get and display weather information.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// ------------------------
 // Get background image
 fetch(
   "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
@@ -22,6 +23,7 @@ fetch(
     document.getElementById("img-author").textContent = `By: Sarah Dorweiler`;
   });
 
+// ------------------------
 // Get Crypto data
 fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
   .then((response) => {
@@ -36,13 +38,56 @@ fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
     const logo = data.image.thumb;
     // console.log(data);
 
-    document.querySelector(".crypto-data").innerHTML = `
-    <img src="${logo}" alt="" />
+    document.querySelector(".crypto").innerHTML = `
+    <img class="crypto__logo" src="${logo}" alt="${data.name} logo" />
     <span>$${price}</span>
     `;
   })
   .catch((error) => console.log(error.stack));
 
+// ------------------------
+// Display weather info
+
+function successfulPosition(position) {
+  // Get user location and display weather information
+  const { latitude, longitude } = position.coords;
+  getWeather(latitude, longitude);
+}
+
+function errorPosition(error) {
+  console.log(`ErrorCode ${error.code}: ${error.message}`);
+}
+
+navigator.geolocation.getCurrentPosition(successfulPosition, errorPosition, {
+  enableHighAccuracy: true
+});
+
+async function getWeather(latitude, longitude) {
+  try {
+    const response = await fetch(
+      `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric`
+    );
+    if (!response.ok) throw Error(response.status);
+
+    const data = await response.json();
+    // console.log({ data });
+    const temp = data.main.temp;
+    const cityName = data.name;
+    const icon = data.weather[0].icon;
+
+    document.querySelector(".weather").innerHTML = `
+    <div class="weather__temperature">
+      <img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="" />
+      <span>${Math.round(temp)} °C</span>
+    </div>
+    <p>${cityName}</p>
+    `;
+  } catch (error) {
+    console.log(error.stack);
+  }
+}
+
+// ------------------------
 // Get and display current time
 function getCurrentTime() {
   const date = new Date();
